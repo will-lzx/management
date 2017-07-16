161c6dee5ed493a58da154ca0c396e991dc8a8aa
will-lzx <Password01!>
2017-07-16 10:17:53 +0800

    管理后台设计

--- static/js/relalive.js
+++ static/js/relalive.js
@@ -1,4 +1,4 @@
 function showTip(tip, type) {
     var $tip = $('#tip');
-    $tip.attr('class', 'alert alert-' + type).text(tip).css('margin-left', - $tip.outerWidth(1500) / 2).fadeIn(500).delay(8000).fadeOut(500);
+    $tip.attr('class', 'alert alert-' + type).text(tip).css('margin-left', - $tip.outerWidth(1500) / 2).fadeIn(500).delay(4000).fadeOut(500);
 }
